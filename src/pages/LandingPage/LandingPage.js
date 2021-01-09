ee805002dad705b42735d67905acb612bf6cccdb
Yasmine <yasszmine@Radis-MacBook-Pro.local>
2021-01-10 00:46:29 +0100

    refactor(): Features component added

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Headline from '../../components/Headline/Headline';
+import Features from '../../components/KeyFeatures/Features';
 
 function LandingPage() {
  return (
    <div className="landing-page">
      <Headline />
+     <Features />
    </div>
  );
 }
