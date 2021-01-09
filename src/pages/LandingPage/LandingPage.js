f6ab164925400f2bd211033fabdabd20a791a697
Yasmine <yasszmine@Radis-MacBook-Pro.local>
2021-01-09 23:56:05 +0100

    refactor(): Headline component added to page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Headline from '../../components/Headline/Headline';
 
 function LandingPage() {
  return (
    <div className="landing-page">
+     <Headline />
    </div>
  );
 }
