8e9aae4744ed877186a344e1c914bea923906189
Yasmine <yasszmine@Radis-MacBook-Pro.local>
2021-01-09 14:27:05 +0100

    refactor(): router added and route to landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Header from './components/Header/Header';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
