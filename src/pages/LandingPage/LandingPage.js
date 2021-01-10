339146d0a4402dad32aabaa0efc48a50c4867582
Yasmine <yasszmine@Radis-MacBook-Pro.local>
2021-01-10 01:36:57 +0100

    refactor(): demo component added

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Headline from '../../components/Headline/Headline';
 import Features from '../../components/KeyFeatures/Features';
+import Demo from '../../components/Demo/Demo';
 
 function LandingPage() {
  return (
    <div className="landing-page">
      <Headline />
      <Features />
+     <Demo />
    </div>
  );
 }
